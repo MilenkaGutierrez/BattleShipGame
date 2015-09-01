df9df9c89a7bc78248ba3821a755c4220a07f8d4
milenkagutierrez <milenka.gutierrez@fundacion-jala.org>
2015-09-01 14:51:52 -0400

    classes game

---
+++ game/app/js/player.js
@@ -0,0 +1,8 @@
+/**
+ * Created by milenkagutierrez on 9/1/2015.
+ */
+var Player = function(name){
+    this.name = name;
+    this.table = new Table();
+    this.numShots = 0;
+}
---
+++ game/app/js/table.js
@@ -0,0 +1,7 @@
+/**
+ * Created by milenkagutierrez on 9/1/2015.
+ */
+var Table = function(){
+    var size = Number;
+    var ship = [Ship];
+}
