41ed0a1234ce31c52157a5b8cd6e17caec37cf1c
JoaquinGonzales <joaquinjqn2@gmail.com>
2015-09-12 13:13:09 -0400

    classes game

--- game/app/js/game.js
+++ game/app/js/game.js
@@ -12,22 +12,34 @@ var Game = function(name){
 Game.prototype.start = function(){
     console.log(this.name,'game started');
     console.log('Here should start the game logic');
-    console.log('TMP: table', this.players[0].table._field);
 
-    var numShots = 0
+    do {
+        var size = window.prompt('Table size (Min 8 cells)? eg. 4,6');
+        var num = size.split(',');
+    }while((num[0]*num[1])<8);
+
+    this.players[0].createTable(num[0], num[1]);
+
+    /*this.players[0].table._createShips();
+    this.players[0].table._initField();
+    this.players[0].table._placeShips();*/
+
+    var numShots = 0;
+    var maxShots = parseInt((num[0]*num[1])/3);
     do{
-        var pos = this._doShot();
-        numShots++;
-        console.log('TMP: table');
-        console.log(this.players[0].table._field);
-    }while(numShots < 3);
+        var pos = this.players[0].table._doShot();
+        if(pos == 1)
+            numShots++;
+    }while(numShots < maxShots);
+
+    console.log('The game finished');
+    console.log('TMP: table', this.players[0].table._field);
 }
 
 //window.confirm() para aceptar o rechazar
 //window.prompt() para introducir valores desde consola
 
 Game.prototype._createPlayers = function(){
-    //TODO: Number of players should be retrieved from a constant
     var numPlayers = 1;
     for(var i = 0; i < numPlayers; i++){
         var player = new Player('Player' + i);
